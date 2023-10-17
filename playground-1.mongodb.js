98658cd28fe1a98922ef758aec07c9fd7f2b850d
Alessandro Lima <alessandroslima@hotmail.com>
2023-10-17 11:22:21 -0700

    Q: In users collection find average age of users by status for the users who joined in 2013.

---
+++ playground-1.mongodb.js
@@ -0,0 +1,16 @@
+// Q: In users collection find average age of users by 
+// status for the users who joined in 2013.
+
+db.users.aggregate([
+  {$match: {
+      "join_date": /^2013/
+  }},
+  {$group: {
+    _id: {
+      status: "$status"
+  },
+    avgAge: {
+      $avg: "$age"
+    }
+  }}
+])
